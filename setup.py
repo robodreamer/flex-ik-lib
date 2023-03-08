e8bbaec596accfd51e403bdefe5c5060af68f15d
Andy <andypark@theaiinstitute.com>
2023-03-08 12:00:30 -0500

    updated classifiers for setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
-DESCRIPTION = 'Flexible IK Solver for Redundant Manipulators'
-LONG_DESCRIPTION = \
-    'This package contains a flexible inverse kinematics solver for redundant manipulators. \
+VERSION = "0.0.1"
+DESCRIPTION = "Flexible IK Solver for Redundant Manipulators"
+LONG_DESCRIPTION = "This package contains a flexible inverse kinematics solver for redundant manipulators. \
     It is based on the SNS-IK algorithm, which is a multi-task prioritization algorithm \
-    that can handle joint limits, joint velocity limits, and task velocity limits.'
+    that can handle joint limits, joint velocity limits, and task velocity limits."
 
 # Setting up
 setup(
-       # the name must match the folder name 'flexIk'
-        name="flexIk",
-        version=VERSION,
-        author="Andy Park",
-        author_email="<andypark.purdue@email.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=["numpy"], # add any additional packages that
-        # needs to be installed along with your package. Eg: 'caer'
-
-        keywords=['python', 'ik', 'inverse kinematics', 'sns-ik', 'redundant manipulators'],
-        classifiers= [
-            "Development Status :: 1 - Beta",
-            "Intended Audience :: Research",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-            "Operating System :: POSIX :: Linux",
-        ]
+    # the name must match the folder name 'flexIk'
+    name="flexIk",
+    version=VERSION,
+    author="Andy Park",
+    author_email="<andypark.purdue@email.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["numpy"],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=["python", "ik", "inverse kinematics", "sns-ik", "redundant manipulators"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
